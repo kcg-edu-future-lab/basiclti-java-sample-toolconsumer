e53a223bbb38f66433326b8bb0a81222b88d2ea2
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2021-11-29 00:04:42 +0900

    fungoのLTIToolに遷移するサンプル。

--- src/main/java/edu/kcg/futurelab/sample/springboot/controller/GoController.java
+++ src/main/java/edu/kcg/futurelab/sample/springboot/controller/GoController.java
@@ -1,16 +1,30 @@
 package edu.kcg.futurelab.sample.springboot.controller;
 
+import java.util.HashMap;
+import org.imsglobal.lti.launch.LtiOauthSigner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/go")
 public class GoController {
+    private static String url = "https://fungo.kcg.edu/ltitest/launchweb";
+    private static String key = "930bea11-d1b3-c5f6-6629-754d2a47e454";
+    private static String secret = "secret";
 	@GetMapping()
-	public String go() {
-		return "users/index";
+	public String go(Model model) throws Throwable{
+        var parameters = new HashMap<String, String>(){{
+            put("user_id", "userId");
+            put("context_id", "contextId");
+            put("user_name", "userName");
+        }};
+        var signer = new LtiOauthSigner();
+        var signedParameters = signer.signParameters(
+            parameters, key, secret, url, "POST");
+        model.addAttribute("url", url);
+        model.addAttribute("params", signedParameters);
+		return "go";
 	}
 }
